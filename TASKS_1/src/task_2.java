c6ec2ac414b98ffd8e518af778408f54da12b0fb
janetjk <janetjk@metropolia.fi>
2026-01-13 19:04:27 +0200

    tasks 1 task 2

--- TASKS_1/src/task_2.java
+++ TASKS_1/src/task_2.java
@@ -1,2 +1,16 @@
+/*
+    Longer ugly version
 public class task_2 {
+    public static void main(String[]args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+    }
+}
+ */
+//pretty short version
+public class task_2 {
+    public static void main(String[]args) {
+        System.out.println("    *\n   ***\n  *****\n *******");}
 }
