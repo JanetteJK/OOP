f741b63ba855fbe89c7c287d1a6da931d2bbec84
janetjk <janetjk@metropolia.fi>
2026-01-13 15:00:21 +0200

    planning and tests

---
+++ src/calculation.java
@@ -0,0 +1,12 @@
+public class calculation {
+    public static void main(String[] args) {
+        int a = 4;
+
+        System.out.println(a++); // a = a + 1
+        System.out.println(a++);
+        a = a + 10;
+        a += 10;
+        a *= 10; //a = 10 * a
+        System.out.println(a);
+    }
+}
---
+++ src/divide.java
@@ -0,0 +1,9 @@
+public class divide {
+    public static void main(String[] args ){
+        int a = 4;
+        double b = 3.0; //if we want decimals
+
+        System.out.println(a / b);
+    }
+}
+
