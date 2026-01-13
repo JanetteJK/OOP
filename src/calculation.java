5850d224a7e0ece3da3bb8bbd4617f0edf406fb9
janetjk <janetjk@metropolia.fi>
2026-01-13 18:49:56 +0200

    planning and tests

--- src/calculation.java
+++ src/calculation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class calculation {
     public static void main(String[] args) {
         int a = 4;
@@ -8,5 +10,19 @@ public class calculation {
         a += 10;
         a *= 10; //a = 10 * a
         System.out.println(a);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double firstnumber = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        double sumOfSquares = Math.pow(firstnumber, 2) + Math.pow(secondNumber, 2);
+
+        System.out.println("\n");
+        System.out.printf("The sum of squares is: %6.2f\n", sumOfSquares); // f = print formatted
     }
 }
+
