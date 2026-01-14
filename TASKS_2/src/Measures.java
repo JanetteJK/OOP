4f9f037edf85bbc44ee414b587bd3f85cab8fde8
janetjk <janetjk@metropolia.fi>
2026-01-14 18:45:07 +0200

    tasks 2, task 3 started

---
+++ TASKS_2/src/Measures.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Measures {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me a weight in grams: ");
+        double grams = scanner.nextDouble();
+
+        double luoti = 13.28;
+        double naula = luoti *= 32;
+        double leiviska = grams /= (naula *= 20);
+
+
+
+
+
+        System.out.println(grams + " grams is " +leiviska+ " leiviskää, " +naula+ " naulaa and " +luoti+ "luotia");
+    }
+}
