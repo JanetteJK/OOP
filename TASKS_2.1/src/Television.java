7868bcd7aa7107dc6fbc9a8fbbc79516327c92be
janetjk <janetjk@metropolia.fi>
2026-01-22 22:01:50 +0200

    television task done

---
+++ TASKS_2.1/src/Television.java
@@ -0,0 +1,35 @@
+public class Television {
+    private int channel;
+    private boolean on = false;
+
+    public void Television() {
+        channel = 0;
+    }
+
+    void setChannel(int number) {
+        channel = number;
+    }
+
+    boolean isOn() {
+        if (on) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    void pressOnOff() {
+        if (on){
+            on = false;
+        }
+    }
+
+    public int getChannel() {
+        if (channel > 10){
+            channel = 1;
+        }
+        return channel;
+    }
+}
