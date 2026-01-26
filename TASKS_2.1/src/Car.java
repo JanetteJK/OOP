63274d1833dbe330fe33da7a72a2624982987726
janetjk <janetjk@metropolia.fi>
2026-01-26 16:15:30 +0200

    car and car driver started

--- TASKS_2.1/src/Car.java
+++ TASKS_2.1/src/Car.java
@@ -4,12 +4,49 @@ public class Car {
     private double gasolineLevel;
     private String typeName;
     private double gasolineTankCapacity;
+    private boolean cruiseControl;
+    private double targetSpeed;
+    private boolean on = false;
 
-    public Car(String typeName) {
+
+    public Car(String typeName, int gasolineTankCapacity) {
         speed = 0;
         gasolineLevel = 0;
         this.typeName = typeName;
-        gasolineTankCapacity = 40;
+        this.gasolineTankCapacity = gasolineTankCapacity;
+        cruiseControl = cruiseControl();
+        targetSpeed = 0;
+    }
+
+    public void turnOn(int setting){
+        if (setting == 0) {
+            on = false;
+        }
+        if (setting == 1) {
+            on = true;
+        }
+    }
+
+    public boolean cruiseControl() {
+        if (on) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void setTargetSpeed(double max) {
+        if (max > 100 || max < 50) {
+            System.out.println("Target speed outside of range, turning off cruise control");
+            on = false;
+        }
+        targetSpeed = max;
+
+    }
+
+    public double getTargetSpeed() {
+        return targetSpeed;
     }
 
     public void accelerate() {
@@ -19,7 +56,7 @@ public class Car {
             speed = 0;
     }
 
-    void decelerate(int amount) {
+    void decelerate(double amount) {
         if (gasolineLevel > 0) {
             if (amount > 0)
                 speed = Math.max(0, speed - amount);
@@ -36,7 +73,7 @@ public class Car {
     }
 
     void fillTank() {
-        gasolineLevel = 100;
+        gasolineLevel = gasolineTankCapacity;
     }
 
     double getGasolineLevel() {
--- TASKS_2.1/src/CarDriver.java
+++ TASKS_2.1/src/CarDriver.java
@@ -2,8 +2,23 @@ public class CarDriver {
     public static void main(String[] args) {
         Car myCar;
 
-        myCar = new Car("Toyota Corolla");
+        myCar = new Car("Toyota Corolla", 80);
         myCar.fillTank();
+        myCar.turnOn(1);
+
+        while (myCar.cruiseControl()) {
+            myCar.setTargetSpeed(100);
+            System.out.println("Current target speed is: " + myCar.getTargetSpeed() + " km/h");
+            System.out.println(myCar.getSpeed());
+            if (myCar.getSpeed() > myCar.getTargetSpeed()) {
+                myCar.decelerate(10);
+            }
+            else if (myCar.getSpeed() < myCar.getTargetSpeed()) {
+                myCar.accelerate();
+            }
+
+        }
+
 
         for (int i = 0; i < 6; i++) {
             myCar.accelerate();
