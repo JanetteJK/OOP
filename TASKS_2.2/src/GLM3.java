59f79e86a7510b98c4f311c9b7a69ef89b2c2dee
janetjk <janetjk@metropolia.fi>
2026-01-28 22:24:55 +0200

    grocerylist manager 3,4,5 added to the same GLU/GLM files

--- TASKS_2.2/src/GLM3.java
+++ TASKS_2.2/src/GLM3.java
@@ -4,17 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GLM3 {
+    public class Item {
+        private double cost;
+        private String category;
+        private int amount;
+
+        public Item(double cost, String category, int amount){
+            this.cost = cost;
+            this.category = category;
+            this.amount = amount;
+        }
+        public double getCost(){
+            return cost;
+        }
+        public String getCategory(){
+            return category;
+        }
+        public int getAmount(){
+            return amount;
+        }
+        public void setAmount(int amount){
+            this.amount = amount;
+        }
+    }
+
+    private HashMap<String, Item> groceryList = new HashMap<>();
 
 
-    private HashMap<String, Groceries> groceryList = new HashMap<>();
-
-
-    public void addItem(String item, Groceries) {
+    public void addItem(String item, double cost, String category, int amount) {
         if (groceryList.containsKey(item)) {
             System.out.println(item + " is already on the list.\n");
         } else {
             System.out.println("Adding " + item + " to the list.\n");
-            groceryList.put(item, Groceries);
+            groceryList.put(item, new Item(cost, category, amount));
         }
     }
 
@@ -29,7 +51,7 @@ public class GLM3 {
 
     public void displayList() {
         System.out.println("Grocery list:");
-        for (Map.Entry<String, Double> entry : groceryList.entrySet()) {
+        for (Map.Entry<String, Item> entry : groceryList.entrySet()) {
             String item = entry.getKey();
             System.out.println(item);
         }
@@ -42,22 +64,37 @@ public class GLM3 {
 
     public void calculateTotalCost() {
         double total = 0;
-        for (Map.Entry<String, Double> entry : groceryList.entrySet()) {
-            double value = entry.getValue();
+        for (Map.Entry<String, Item> entry : groceryList.entrySet()) {
+            int times = entry.getValue().getAmount();
+            double price = entry.getValue().getCost();
+            double value = times * price;
             total = total + value;
         }
         System.out.println("The total value of the groceries is: " + total + "€");
     }
 
     public void displayByCategory(String category) {
+        System.out.println(category + ": ");
+        for (Map.Entry<String, Item> entry : groceryList.entrySet()){
+            if (entry.getValue().getCategory().equals(category)) {
+                String item = entry.getKey();
+                System.out.println(item);
+            }
+        }
 
     }
 
-    public class Groceries {
-        public Groceries(double cost, String category) {
+    public void updateAmount(String item, int newAmount){
+        if (groceryList.containsKey(item)){
+            groceryList.get(item).setAmount(newAmount);
+            System.out.println("There are now " + newAmount + " " + item + " on the list");
+        } else {
+            System.out.println("Item not on list.");
         }
     }
+
 }
+
 
 
 
--- TASKS_2.2/src/GLU3.java
+++ TASKS_2.2/src/GLU3.java
@@ -3,21 +3,31 @@ import java.util.Scanner;
 
 public class GLU3 {
     public static void main(String[] args) {
-        GLM2 list;
-        list = new GLM2();
+        GLM3 list;
+        list = new GLM3();
 
         list.displayList();
-
-        list.addItem("Apples", 2, "fruits");
-        list.addItem("Milk", 4.4, "dairy");
-        list.addItem("Bread", 5.5,"bakery");
-        list.addItem("Apples" ,2.0, "fruits");
-
-        list.removeItem("Apples");
-
+        //add items to list
+        list.addItem("Apples", 2, "fruits", 2);
+        list.addItem("Milk", 4.4, "dairy", 1);
+        list.addItem("Bread", 5.5,"bakery", 1);
+        list.addItem("Oranges" ,2.0, "fruits", 6);
+        list.addItem("Bananas", 0.9, "fruits", 3);
+        // show list
         list.displayList();
 
+        // remove items and show list again
+        list.removeItem("Oranges");
+        list.removeItem("Bread");
+        list.displayList();
+        // show categories
+        list.displayByCategory("fruits");
+        list.displayByCategory("dairy");
+        // calculate cost of all items on list
         list.calculateTotalCost();
+        //
+        list.updateAmount("Oranges", 3);
+        list.updateAmount("Milk", 3);
 
 
 
