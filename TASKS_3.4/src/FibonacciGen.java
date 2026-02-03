c5f3f2c2aaf80a708f5415e86e810916eb397ee4
janetjk <janetjk@metropolia.fi>
2026-02-03 17:31:00 +0200

    fibonacci num generator works

--- TASKS_3.4/src/FibonacciGen.java
+++ TASKS_3.4/src/FibonacciGen.java
@@ -12,33 +12,25 @@ public class FibonacciGen {
         this.nextNumber = 1;
         this.maxNumber = maxNumber;
 
-
-        for (int i = 0; i < maxNumber; i++) {
+        try {
+            Writer writer = new FileWriter("TASKS_3.4/src/Fibonaccinumber.csv");
+        for (int i = 0; i <= maxNumber; i++) {
             //numbers.add(prevNumber);
-            long number = prevNumber + nextNumber;
+            writer.write(String.valueOf((long) this.prevNumber +"\n"));
+            writer.flush();
+            long number = this.prevNumber + this.nextNumber;
             System.out.println(prevNumber);
             this.prevNumber = nextNumber;
             this.nextNumber = number;
 
 
         }
-        try {
-            Writer writer = new FileWriter("TASKS_3.4/src/Fibonaccinumber.csv");
-            for (int i = 0; i < numbers.size(); i++) {
-                writer.write(String.valueOf((long) numbers.get(i)));
-                writer.flush();
-            }
-        }
-        catch(IOException e){
+            }catch(IOException e){
             e.printStackTrace();
         }
+
+
     }
-
-
-
-
-
-
 
         public static void main (String[]args){
         new FibonacciGen(60);
